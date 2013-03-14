9cf59b373a8d09f5228f6abea4535b4a4509caa9
Zack Hovatter <zackhovatter@gmail.com>
2013-03-13 22:36:32 -0500

    Add default cassette options for VCR
    Record new episodes and only match requests on path

--- spec/lib/expedia/client_spec.rb
+++ spec/lib/expedia/client_spec.rb
@@ -3,7 +3,7 @@ require_relative "../../spec_helper"
 describe Expedia::Client do
   describe "#hotel_list" do
     it "returns a hash containing the response from the API" do
-      VCR.use_cassette("hotel_list", :record => :new_episodes) do
+      VCR.use_cassette("hotel_list") do
         response = $client.hotel_list(:destination_string => "Athens, TN")
         response.has_key?(:hotel_list).must_equal true
       end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,10 @@ require "vcr"
 VCR.configure do |c|
   c.cassette_library_dir = "spec/fixtures/expedia_cassettes"
   c.hook_into :webmock
+  c.default_cassette_options = {
+    :record => :new_episodes,
+    :match_requests_on => [:path]
+  }
 end
 
 EAN_CID           = "REPLACEME"
